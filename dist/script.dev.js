36a3ad474f1220125a1d78ba45d1b24850063f43
san.x0000@gmail.com <san.x0000@gmail.com>
2022-11-30 23:28:20 -0300

    iniciado JS e definido padrão do botão slider

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -1,2 +1,15 @@
 "use strict";
+
+var sliderElement = document.querySelector("#slider");
+var buttonElement = document.querySelector("#button");
+var sizePassword = document.querySelector("#valor");
+var password = document.querySelector("#password");
+var containerPassword = document.querySelector("#container-password");
+var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@";
+var novaSenha = "";
+sizePassword.innerHTML = sliderElement.value;
+
+slider.oninput = function () {
+  sizePassword.innerHTML = this.value;
+};
 //# sourceMappingURL=script.dev.js.map
---
+++ script.js
@@ -0,0 +1,17 @@
+
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container-password");
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@"
+let novaSenha = "";
+
+sizePassword.innerHTML = sliderElement.value;
+
+slider.oninput = function () {
+    sizePassword.innerHTML = this.value;
+}
