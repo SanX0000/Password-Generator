990856f507e9bf8fe2e414b3bdeeecb7b4726e84
san.x0000@gmail.com <san.x0000@gmail.com>
2022-12-03 21:44:08 -0300

    site finalizado

--- dist/script.dev.js
+++ dist/script.dev.js
@@ -6,10 +6,28 @@ var sizePassword = document.querySelector("#valor");
 var password = document.querySelector("#password");
 var containerPassword = document.querySelector("#container-password");
 var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@";
-var novaSenha = "";
+var novaSenha = '';
 sizePassword.innerHTML = sliderElement.value;
 
 slider.oninput = function () {
   sizePassword.innerHTML = this.value;
 };
+
+function generatePassword() {
+  var pass = "";
+
+  for (var i = 0, n = charset.length; i < sliderElement.value; ++i) {
+    pass += charset.charAt(Math.floor(Math.random() * n));
+  }
+
+  console.log(pass);
+  containerPassword.classList.remove("hide");
+  password.innerHTML = pass;
+  novaSenha = pass;
+}
+
+function copyPassword() {
+  navigator.clipboard.writeText(novaSenha);
+  alert("Senha copiada com sucesso!");
+}
 //# sourceMappingURL=script.dev.js.map
